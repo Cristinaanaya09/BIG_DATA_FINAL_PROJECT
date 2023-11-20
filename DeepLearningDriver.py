b5750ee00522240f0069b740a8039b6e20a5fe34
Cristinaanaya09 <79280214+Cristinaanaya09@users.noreply.github.com>
2023-11-19 22:20:44 -0600

    Update just python DeepLearningDriver.py

--- DeepLearningDriver.py
+++ DeepLearningDriver.py
@@ -1,58 +1,67 @@
-from mrjob.job import MRJob
-from mrjob.step import MRStep
+import subprocess
 import os
 import random
-from decimal import Decimal
 
-class DeepLearningDriver(MRJob):
+def load_weights_to_cache(weight_file):
+    # Load weights to distributed cache
+    weights = [0.1 * random.gauss(0, 1) for _ in range(numNodeofLayer[0] * numNodeofLayer[1])]
+    with open(weight_file, 'w') as f:
+        f.write(' '.join(map(str, weights)))
 
-    def configure_args(self):
-        super(DeepLearningDriver, self).configure_args()
-        self.add_passthru_arg('--maxEpoch', type=int, help='Maximum number of epochs')
-        self.add_passthru_arg('--numLayer', type=int, help='Number of layers')
-        self.add_passthru_arg('--numNodeofLayer', type=int, nargs='+', help='Number of nodes in each layer')
-        self.add_passthru_arg('--numCase', type=int, help='Number of training items')
+    return weight_file
 
-    def load_weights_to_cache(self, weight_file):
-        # Load weights to distributed cache
-        weights = [0.1 * random.gauss(0, 1) for _ in range(self.options.numNodeofLayer[0] * self.options.numNodeofLayer[1])]
-        with open(weight_file, 'w') as f:
-            f.write(' '.join(map(str, weights)))
+def init_weights():
+    # Initialize weights
+    return [0.1 * random.gauss(0, 1) for _ in range(numNodeofLayer[0] * numNodeofLayer[1])]
 
-        self.add_file_arg('--weights', weight_file)
+def run_map_reduce(input_path, output_path, mapper_class, reducer_class, weights_file=None):
+    # Construct Hadoop Streaming command
+    hadoop_cmd = [
+        'hadoop', 'jar', '/path/to/hadoop-streaming.jar',
+        '-input', input_path,
+        '-output', output_path,
+        '-mapper', mapper_class,
+        '-reducer', reducer_class,
+        '-file', mapper_class,
+        '-file', reducer_class
+    ]
 
-    def init_weights(self):
-        # Initialize weights
-        return [0.1 * random.gauss(0, 1) for _ in range(self.options.numNodeofLayer[0] * self.options.numNodeofLayer[1])]
+    if weights_file:
+        hadoop_cmd.extend(['-cmdenv', f'WEIGHTS_FILE={weights_file}'])
 
-    def run(self):
-        weight_file = 'weights.txt'
-        self.load_weights_to_cache(weight_file)
-
-        for layer in range(self.options.numLayer - 1):
-            # Training phase (RBMMJob)
-            weights = self.init_weights()
-            for iter in range(1, self.options.maxEpoch + 1):
-                weight_file = f'weights-layer-{layer}-iter-{iter}.txt'
-                self.load_weights_to_cache(weight_file)
-
-                # Run RBMMJob
-                self.mr_job = RBMMJob(args=['-r', 'hadoop', '--weights', weight_file])
-                with self.mr_job.make_runner() as runner:
-                    runner.run()
-
-                # Update weights based on output
-
-            # Propagation phase (OPTMRJob)
-            weight_file = f'weights-layer-{layer}-iter-{self.options.maxEpoch}.txt'
-            self.load_weights_to_cache(weight_file)
-
-            # Run OPTMRJob
-            self.mr_job = OPTMRJob(args=['-r', 'hadoop', '--weights', weight_file])
-            with self.mr_job.make_runner() as runner:
-                runner.run()
-
-            # Move to the next layer
+    subprocess.run(hadoop_cmd, check=True)
 
 if __name__ == '__main__':
-    DeepLearningDriver.run()
+    # Argument format: /input_file/ /output_file/ maxiter layernum nodeNumofLayer1 nodeNumofLayer2 ...
+    input_file = '/path/to/input'
+    output_file = '/path/to/output'
+    max_epoch = 10
+    num_layer = 3
+    numNodeofLayer = [input() for _ in range(num_layer)]
+
+    num_case = 60000
+
+    epoch_in_mapper = 1
+
+    use_distributed_cache = "1"
+
+    for layer in range(num_layer - 1):
+        # Training phase (RBMMJob)
+        weights = init_weights()
+        for iter in range(1, max_epoch + 1):
+            weight_file = f'weights-layer-{layer}-iter-{iter}.txt'
+            load_weights_to_cache(weight_file)
+
+            # Run RBMMJob
+            run_map_reduce(input_file, output_file, 'RBMMapper.py', 'RBMReducer.py', weights_file=weight_file)
+
+            # Update weights based on output
+
+        # Propagation phase (OPTMRJob)
+        weight_file = f'weights-layer-{layer}-iter-{max_epoch}.txt'
+        load_weights_to_cache(weight_file)
+
+        # Run OPTMRJob
+        run_map_reduce(input_file, output_file, 'OptMapper.py', 'OptReducer.py', weights_file=weight_file)
+
+        # Move to the next layer
