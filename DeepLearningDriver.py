ff3bc5ac3faa7ec4640e8e84f44d526ca4625d7a
Cristinaanaya09 <79280214+Cristinaanaya09@users.noreply.github.com>
2023-11-19 23:08:09 -0600

    Update DeepLearningDriver.py

--- DeepLearningDriver.py
+++ DeepLearningDriver.py
@@ -2,17 +2,17 @@ import subprocess
 import os
 import random
 
-def load_weights_to_cache(weight_file):
+def load_weights_to_cache(weight_file, num_node_of_layer):
     # Load weights to distributed cache
-    weights = [0.1 * random.gauss(0, 1) for _ in range(numNodeofLayer[0] * numNodeofLayer[1])]
+    weights = [0.1 * random.gauss(0, 1) for _ in range(num_node_of_layer[0] * num_node_of_layer[1])]
     with open(weight_file, 'w') as f:
         f.write(' '.join(map(str, weights)))
 
     return weight_file
 
-def init_weights():
+def init_weights(num_node_of_layer):
     # Initialize weights
-    return [0.1 * random.gauss(0, 1) for _ in range(numNodeofLayer[0] * numNodeofLayer[1])]
+    return [0.1 * random.gauss(0, 1) for _ in range(num_node_of_layer[0] * num_node_of_layer[1])]
 
 def run_map_reduce(input_path, output_path, mapper_class, reducer_class, weights_file=None):
     # Construct Hadoop Streaming command
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     output_file = '/path/to/output'
     max_epoch = 10
     num_layer = 3
-    numNodeofLayer = [input() for _ in range(num_layer)]
+    num_node_of_layer = [int(input()) for _ in range(num_layer)]  # Modify the input method if needed
 
     num_case = 60000
 
@@ -47,21 +47,22 @@ if __name__ == '__main__':
 
     for layer in range(num_layer - 1):
         # Training phase (RBMMJob)
-        weights = init_weights()
+        weights = init_weights(num_node_of_layer)
         for iter in range(1, max_epoch + 1):
             weight_file = f'weights-layer-{layer}-iter-{iter}.txt'
-            load_weights_to_cache(weight_file)
+            load_weights_to_cache(weight_file, num_node_of_layer)
 
             # Run RBMMJob
-            run_map_reduce(input_file, output_file, 'RBMMapper.py', 'RBMReducer.py', weights_file=weight_file)
-
+            run_map_reduce(input_file, output_file, 'RBMMJob.py', weights_file=weight_file)
+            
             # Update weights based on output
-
+            
         # Propagation phase (OPTMRJob)
         weight_file = f'weights-layer-{layer}-iter-{max_epoch}.txt'
-        load_weights_to_cache(weight_file)
-
-        # Run OPTMRJob
-        run_map_reduce(input_file, output_file, 'OptMapper.py', 'OptReducer.py', weights_file=weight_file)
+        load_weights_to_cache(weight_file, num_node_of_layer)
+        
+        # Run OPTMRJob (assuming OPTMRJob.py contains both mapper and reducer logic)
+        run_map_reduce(input_file, output_file, 'OPTMRJob.py', weights_file=weight_file)
 
         # Move to the next layer
+
